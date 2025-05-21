17e30b002d1992f61c01b91c3a8698d2e6f36f3b
janie20 <janie@iradar.com.my>
2025-05-21 19:42:26 +0800

    add static image

--- kiratech_inventory/settings.py
+++ kiratech_inventory/settings.py
@@ -123,7 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
